887f55c4984850f3bc4282e67c9849b41cb9b3a3
kuivienen <filippovse@gmail.com>
2017-10-30 15:54:53 +0300

    Для восстановления

--- Library/pwm.h
+++ Library/pwm.h
@@ -16,79 +16,103 @@
                                           НАСТРОЙКИ ТАЙМЕРОВ
 **************************************************************************************************/
 
-/**********настройка таймера для первого и второго канала управления*/
+/******НАСТРОЙКА ТАЙМЕРА ДЛЯ ПЕРВОГО КАНАЛА УПРАВЛЕНИЯ****/
+#define TIMER_1
+	#define TIMER1_REGS						TIM1
+//****************указываем выходные пины******************
+#define TIMER1_CH1	
+	#define TIMER1_PORT_CH1				GPIOA
+	#define TIMER1_PIN_CH1				GPIO_Pin_8
+#define TIMER1_CH2	
+	#define TIMER1_PORT_CH2				GPIOA
+	#define TIMER1_PIN_CH2				GPIO_Pin_9
+#define TIMER1_CH3
+	#define TIMER1_PORT_CH3				GPIOA
+	#define TIMER1_PIN_CH3				GPIO_Pin_10
+#define TIMER1_CH4
+	#define TIMER1_PORT_CH4				GPIOA
+	#define TIMER1_PIN_CH4				GPIO_Pin_11
+//**********************************************************
+	#define TIMER1_IRQ						TIM1_UP_IRQn					//	флаг прерывания
+	#define TIMER1_FREQ_SIG				50000									//	частота модулирующего сигнала
+	#define TIMER1_FREQ_PWM				3000									//	частота огибающей	
+
+
+/******НАСТРОЙКА ТАЙМЕРА ДЛЯ ВТОРОГО КАНАЛА УПРАВЛЕНИЯ****/
 #define TIMER_2
 	#define TIMER2_REGS						TIM2
-	
-//*********пины комплиментарных выходов CH***********
-
+//****************указываем выходные пины******************
 #define TIMER2_CH1	
 	#define TIMER2_PORT_CH1				GPIOA
-	#define TIMER2_PIN_CH1				GPIO_Pin_8
-
+	#define TIMER2_PIN_CH1				GPIO_Pin_15
 #define TIMER2_CH2	
-	#define TIMER2_PORT_CH2				GPIOA
-	#define TIMER2_PIN_CH2				GPIO_Pin_9
-	
+	#define TIMER2_PORT_CH2				GPIOB
+	#define TIMER2_PIN_CH2				GPIO_Pin_3
 #define TIMER2_CH3
 	#define TIMER2_PORT_CH3				GPIOA
 	#define TIMER2_PIN_CH3				GPIO_Pin_10
-	
 #define TIMER2_CH4
 	#define TIMER2_PORT_CH4				GPIOA
 	#define TIMER2_PIN_CH4				GPIO_Pin_11
-	
-//--------------------------------------------------
-
-//	флаг прерывания 
-	#define TIMER2_IRQ						TIM2_IRQn
-//	частота модулирующего сигнала
-	#define TIMER2_FREQ_SIG				50000
-//	частота огибающей	
-	#define TIMER2_FREQ_PWM				3000
+//**********************************************************
+	#define TIMER2_IRQ						TIM2_IRQn							//	флаг прерывания 
+	#define TIMER2_FREQ_SIG				50000									//	частота модулирующего сигнала
+	#define TIMER2_FREQ_PWM				3000									//	частота огибающей	
 	
 	
-/******настройка таймера для третьего и четвертого канала управления****/	
+/******НАСТРОЙКА ТАЙМЕРА ДЛЯ ТРЕТЬЕГО КАНАЛА УПРАВЛЕНИЯ****/
+#define TIMER_3
+	#define TIMER3_REGS						TIM3
+//****************указываем выходные пины******************
+#define TIMER3_CH1	
+	#define TIMER3_PORT_CH1				GPIOB
+	#define TIMER3_PIN_CH1				GPIO_Pin_4
+#define TIMER3_CH2	
+	#define TIMER3_PORT_CH2				GPIOB
+	#define TIMER3_PIN_CH2				GPIO_Pin_5
+#define TIMER3_CH3
+	#define TIMER3_PORT_CH3				GPIOB
+	#define TIMER3_PIN_CH3				GPIO_Pin_0
+#define TIMER3_CH4
+	#define TIMER3_PORT_CH4				GPIOA
+	#define TIMER3_PIN_CH4				GPIO_Pin_1
+//**********************************************************
+	#define TIMER3_IRQ						TIM3_IRQn							//	флаг прерывания 
+	#define TIMER3_FREQ_SIG				50000									//	частота модулирующего сигнала
+	#define TIMER3_FREQ_PWM				3000									//	частота огибающей	
+	
+	
+/******НАСТРОЙКА ТАЙМЕРА ДЛЯ ЧЕТВЕРТОГО КАНАЛА УПРАВЛЕНИЯ****/
 #define TIMER_4
 	#define TIMER4_REGS						TIM4
-
-//*********пины комплиментарных выходов CH***********
-
+//****************указываем выходные пины******************
 #define TIMER4_CH1	
 	#define TIMER4_PORT_CH1				GPIOB
 	#define TIMER4_PIN_CH1				GPIO_Pin_6
-
 #define TIMER4_CH2	
 	#define TIMER4_PORT_CH2				GPIOB
 	#define TIMER4_PIN_CH2				GPIO_Pin_7
-	
 #define TIMER4_CH3
 	#define TIMER4_PORT_CH3				GPIOB
 	#define TIMER4_PIN_CH3				GPIO_Pin_8
-	
 #define TIMER4_CH4
-	#define TIMER4_PORT_CH4				GPIOB
+	#define TIMER4_PORT_CH4				GPIOA
 	#define TIMER4_PIN_CH4				GPIO_Pin_9
-	
-//--------------------------------------------------
-
-//	флаг прерывания 
-	#define TIMER4_IRQ						TIM4_IRQn
-//	частота модулирующего сигнала
-	#define TIMER4_FREQ_SIG				50000
-//	частота огибающей	
-	#define TIMER4_FREQ_PWM				3000
+//**********************************************************
+	#define TIMER4_IRQ						TIM4_IRQn							//	флаг прерывания 
+	#define TIMER4_FREQ_SIG				50000									//	частота модулирующего сигнала
+	#define TIMER4_FREQ_PWM				3000									//	частота огибающей	
 
 /**********настройка таймера для формирования ускорения и торможения*******/	
 
-#define TIMER_3
+/*#define TIMER_3
 	#define TIMER3_REGS						TIM3
 //	флаг прерывания 
 	#define TIMER3_IRQ						TIM3_IRQn
 	#define TIMER3_CH1						&Timer2
 	#define TIMER3_CH2						&Timer2
 	#define TIMER3_CH3						&Timer4
-	#define TIMER3_CH4						&Timer4
+	#define TIMER3_CH4						&Timer4*/
 	
 	
 
